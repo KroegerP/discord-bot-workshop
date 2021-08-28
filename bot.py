04dc4d7a1ff11b81878960a8d176017b48b777c4
Peter Kroeger <39171216+KroegerP@users.noreply.github.com>
2021-08-27 20:11:32 -0400

    Implemented Theme Song

--- bot.py
+++ bot.py
@@ -71,17 +71,24 @@ async def on_voice_state_update(member, before, after):
     print(f'{member} has made a voice state change')
     if (member.bot or after.channel is None):                        # Checking that the bot does not trigger the rest of the code
         return
-
-    if (before.channel == None and after.channel != None):
-        if (member.id == myId):
-            return
-        elif (member.id == 347028600568020992): # If Ben is the person making voice changes
-            voiceChannel = after.channel
-            await voiceChannel.connect()
-            voice = discord.utils.get(client.voice_clients, guild=after.channel.guild)
-            voice.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("audio-testing\\NFL.mp3"), volume=0.1))
-            asyncio.wait(10)
-            voice.stop()
+    songDirectory = f'theme-songs\\{member}'
+    song_there = os.path.isfile(f'{songDirectory}\\themeSong.mp3')
+    if (before.channel == None and after.channel != None and song_there):
+        voiceChannel = after.channel
+        await voiceChannel.connect()
+        voice = discord.utils.get(client.voice_clients, guild=after.channel.guild)
+        myAudio = discord.FFmpegPCMAudio(f'{songDirectory}\\themeSong.mp3')
+        myAdjustedAudio = discord.PCMVolumeTransformer(myAudio, volume=0.1)
+        voice.play(myAdjustedAudio)
+        # asyncio.wait(10)
+        # voice.stop()
+        # elif (member.id == 347028600568020992): # If Ben is the person making voice changes
+        #     voiceChannel = after.channel
+        #     await voiceChannel.connect()
+        #     voice = discord.utils.get(client.voice_clients, guild=after.channel.guild)
+        #     voice.play(discord.PCMVolumeTransformer(discord.FFmpegPCMAudio("audio-testing\\NFL.mp3"), volume=0.1))
+        #     asyncio.wait(10)
+        #     voice.stop()
 
 @client.command()
 async def join(ctx):
@@ -157,15 +164,24 @@ async def addThemeSong(ctx, url : str, member : discord.Member = None):
     songDirectory = f'theme-songs\\{member}'
     print(songDirectory)
     print(url)
-    hasThemeSong = os.path.isdir(f'theme-songs\\{member}')
-    print(hasThemeSong)
-    # if theme_song:
-    #     song_there = os.path.isfile("song.mp3")
-    # try:
-    #     if song_there:
-    #         os.remove("song.mp3")
-    # except PermissionError:
-    #     await ctx.send("Wait for current music to end")
-    #     return
+    hasThemeSongFolder = os.path.isdir(songDirectory)
+    if hasThemeSongFolder:
+        song_there = os.path.isfile(f'{songDirectory}\\themeSong.mp3')
+        try:
+            if song_there:
+                os.remove(f'{songDirectory}\\themeSong.mp3')
+        except PermissionError:
+            await ctx.send("User has a folder, but no song file! Please contact dev.")
+            return
+    else:
+        os.mkdir(f'{songDirectory}')
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+        for file in os.listdir('./'):
+            if file.endswith(".mp3"):
+                os.rename(file, f'{songDirectory}\\themeSong.mp3')
+    print("Theme Song successfully added!")
+    # await ctx.channel.purge(limit = 1)
+    # await client.send("Theme Song Added!")
 
 client.run(os.getenv('GUILD_TOKEN'))
